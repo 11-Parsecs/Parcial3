8191cc95b5f12c65ce55e7978d8a416758b42e5d
Juan <juanpaguevara11@gmail.com>
2022-06-04 19:38:49 -0500

    Se termina el código para la base de datos.

--- Libreria/Lectura.py
+++ Libreria/Lectura.py
@@ -1,96 +1,104 @@
-from matplotlib.pyplot import plot
-from matplotlib.pyplot import xticks
-from matplotlib.pyplot import show
-from csv import reader
+from types import NoneType
+import matplotlib.pyplot as plt
 
 Txt1="No se pueden sumar Contables con diferentes asignaciones."
-
-#Teracaloría, Gigavatio-hora, Kilotoneladas equivalentes del petróleo, Gigaunidades térmicas británicas
-#Kilotón, Terajoules, Kilo-barriles de petróleo
-Unidades={"TCal":2.39006*10**(-13),"GWh":2.7777777*10**(-13),"KTEP":2.38846*10**(-14),"GBTU":9.447817*10**(-13),
-          "kTon":2.388459*10**(-13),"TJ":10**(12),"kBL":3.2982828*10**(-13),"Unidad Original":6.242*10**(18),"Mpc":0.000000000000922}#6.242*10**(18)}
 
 class Contable:
     '''
     Ah
     '''
-    def __init__(self,n,a):
+    def __init__(self,m,n,a,T=""):
+        self.m=m
         self.n=n
         self.a=a
+        self.T=T
     def __str__(self):
-        return str(self.n)+"]->"+str(self.a)
+        return str(self.m)+"}->"+str(self.n)+"]->"+str(self.a)
     def __add__(self,other):
-        if self.n==other.n:
-            return Contable(self.n,self.a+other.a)
+        if self.m==other.m and self.n==other.n:
+            return Contable(self.m,self.n,self.a+other.a)
         else:
             print(Txt1)
     def __truediv__(self,other):
-        if self.n==other.n:
-            return Contable(self.n,self.a/other.a)
+        if self.m==other.m and self.n==other.n:
+            return Contable(self.m,self.n,self.a/other.a,self.T+other.T)
         else:
             print(Txt1)
     def __iadd__(self, other):
         return self+other
 
 class Dato(Contable):
-    def __init__(self,N,n):#,U
+    def __init__(self,N,n,M,m):
         self.N=N
+        self.M=M
         self.n=n
-        #self.U=U
-    def Tomar(self,A,a):
+        self.m=m
+    def Tomar(self,A,a,Txt):
         Cuenta=0
-        U=Contable(self.n,0)
+        U=Contable(self.M,self.n,0)
         f1=A[0].index(self.N)
         f2=A[0].index(a) 
-        #f3=A[0].index(self.U) 
+        f3=A[0].index(self.M)
         for L in A:
-            if L[f1]==self.n:
+            if L[f1]==self.n and L[f3]==self.m:
                 try:
                     Cuenta+=1
-                    U+=Contable(self.n,eval(L[f2]))#/Unidades[L[f3]]*2.7777777*10**(-13))
+                    U+=Contable(self.M,self.n,eval(L[f2]))
                 except:
                     Cuenta+=1
-                    U+=Contable(self.n,0)
-        U=U/Contable(self.n,Cuenta)
+                    U+=Contable(self.M,self.n,0)
+        if Cuenta!=0:
+            U=U/Contable(self.M,self.n,Cuenta,Txt)
+        else:
+            U=NoneType#Contable(self.M,self.n,0)
         return U
 
 class Proceso(Dato):
     def __init__(self,A):
         self.A=A
     def __str__(self):
-        return "["+str(self.A)+"]"
+        return "[{"+str(self.A)+"}]"
     def mostrar(self):
-        Comprobar=True
         if type(self.A)==list:
+            Tiempo=0
             X=[]
             Y=[]
-            for D in self.A:
-                if type(D)==Contable:
-                    X.append(D.n)
-                    Y.append(D.a)
+            for L in self.A:
+                if type(L)==list:
+                    for D in L:
+                        if type(D)==Contable:
+                            X.append(D.T)#D.n)
+                            Y.append(D.a)
+                            Tiempo+=2
+                        else:
+                            break
                 else:
-                    Comprobar=False
-                    break
-            if Comprobar:
-                xticks(rotation=90)
-                plot(X,Y,'-o')
-                show()
-            else:
-                print("Eso no se puede")
+                    print("Eso no se puede")
+            fig=plt.figure()
+            fig.set_figwidth(15)
+            fig.set_figheight(6.5)
+            ax=fig.add_subplot(1,1,1)
+            ax.plot(X,Y,'-og')
+            plt.xticks(rotation=90)
+            plt.grid()
+            plt.show()
         else:
             print("Eso no se puede")
 
 
 Dat=[]
 
-f=open("Combustible.csv", encoding="utf8")
+f=open("C:/Users/Juan Pablo/Downloads/Combustible.csv", encoding="utf8")
 for T in  f:
     Dat.append(T.split(","))
 f.close()
 
-EnergiaP=[]
+Precio=[]
 for k in range(2017,2023):
-    EnergiaP.append(Dato("Periodo",str(k)).Tomar(Dat,"Precio"))#,"Unidades Originales"
+    Anual=[]
+    for l in range(1,13):
+        Anual.append(Dato("Periodo",str(k),"Mes",str(l)).Tomar(Dat,"Precio",str(k)+"-"+str(l)))
+    Precio.append(Anual)
 
-P=Proceso(EnergiaP)
+P=Proceso(Precio)
 P.mostrar()
