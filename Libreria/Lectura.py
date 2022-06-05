c3898c14e97f0a30b5d5ed071b60d66ad2110e12
Juan <juanpaguevara11@gmail.com>
2022-06-04 22:27:53 -0500

    Se hacen algunos comentarios

--- Libreria/Lectura.py
+++ Libreria/Lectura.py
@@ -1,11 +1,14 @@
 from types import NoneType
 import matplotlib.pyplot as plt
+from numpy import linspace
 
 Txt1="No se pueden sumar Contables con diferentes asignaciones."
 
 class Contable:
     '''
-    Ah
+    Crea un tipo de dato que se asemeja a las unidades; sólo se pueden operar los datos
+    con la misma asignación. Sirve para agrupar datos asoaciados a dos variables, por
+    ejemplo, puede ser usado para sumar sólo los datos de precio de febrero del año 2019.
     '''
     def __init__(self,m,n,a,T=""):
         self.m=m
@@ -27,12 +30,25 @@ class Contable:
     def __iadd__(self, other):
         return self+other
 
-class Dato(Contable):
+class Espec_Contable(Contable):
+    '''
+    Contable con sólo una asignación. Por ejemplo, puede ser usado
+    para sumar todos los datos de precio del año 2019.
+    '''
+    def init(self,n,a,T=""):
+        super().__init__(0,n,a,T)
+
+class Dato:
+    '''
+    Relaciona y mezcla los datos correspondientes entre filas y columnas.
+    '''
     def __init__(self,N,n,M,m):
         self.N=N
         self.M=M
         self.n=n
         self.m=m
+    def __str__(self):
+        return str(self.N)+"}->"+str(self.n)+"-s-"+str(self.m)+"<-{"+str(self.M)
     def Tomar(self,A,a,Txt):
         Cuenta=0
         U=Contable(self.M,self.n,0)
@@ -50,15 +66,25 @@ class Dato(Contable):
         if Cuenta!=0:
             U=U/Contable(self.M,self.n,Cuenta,Txt)
         else:
-            U=NoneType#Contable(self.M,self.n,0)
+            U=NoneType
         return U
 
-class Proceso(Dato):
+class Proceso:
+    '''
+    Almacena y opera con Contables de una matriz
+    '''
     def __init__(self,A):
         self.A=A
     def __str__(self):
         return "[{"+str(self.A)+"}]"
-    def mostrar(self):
+    def __setitem__(self,N_Dato,Valor):
+        try:
+            self.A[N_Dato]=Valor
+        except IndexError:
+            self.A.append(Valor)
+    def __getitem__(self,N_Dato):
+        return self.A[N_Dato]
+    def mostrar(self,reg=False):
         if type(self.A)==list:
             Tiempo=0
             X=[]
@@ -74,17 +100,38 @@ class Proceso(Dato):
                             break
                 else:
                     print("Eso no se puede")
+
             fig=plt.figure()
-            fig.set_figwidth(15)
+            fig.set_figwidth(20)
             fig.set_figheight(6.5)
             ax=fig.add_subplot(1,1,1)
             ax.plot(X,Y,'-og')
+            
+            if reg:
+                Xn=[]
+                Sxi=0
+                Sxi2=0
+                Syi=0
+                Sxy=0
+                n=0
+                for yi in Y:
+                    n+=1
+                    Syi+=int(yi)
+                    Sxi+=n
+                    Sxi2+=n**2
+                    Sxy+=n*yi
+                B=(n*Sxy-Sxi*Syi)/(n*Sxi2-(Sxi)**2)
+                A=(Syi-B*Sxi)/n
+                Xn=linspace(0,n+40,n+40)
+                C=B*Xn+A
+                ax.plot(Xn,C,'-or')
+
+            ax.tick_params(axis='x', which='major', labelsize=7)
             plt.xticks(rotation=90)
             plt.grid()
             plt.show()
         else:
             print("Eso no se puede")
-
 
 Dat=[]
 
@@ -93,12 +140,13 @@ for T in  f:
     Dat.append(T.split(","))
 f.close()
 
-Precio=[]
+P=Proceso([])
+In=0
 for k in range(2017,2023):
     Anual=[]
     for l in range(1,13):
         Anual.append(Dato("Periodo",str(k),"Mes",str(l)).Tomar(Dat,"Precio",str(k)+"-"+str(l)))
-    Precio.append(Anual)
+    P[In]=Anual
+    In+=1
 
-P=Proceso(Precio)
-P.mostrar()
+P.mostrar(True)
